2e4fe32bc2c5ca9d2e838c67ce26d68d7f1a9234
sinhro200 <sinhro-200@mail.ru>
2020-03-05 20:09:31 +0300

    Added Cube Spawner

---
+++ Assets/Scripts/CubeSpawner/Spawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    [SerializeField]
+    private float CoolDown;
+
+    private float time;
+
+    SpawnerBody[] spawners;
+    void Start()
+    {
+        spawners = getSpawnerBodies();
+        time = 0;
+        
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time > CoolDown)
+        {
+            SpawnerBody sb = spawners[Random.Range(0, spawners.Length )];
+            sb.spawn();
+
+            time -= CoolDown;
+        }
+    }
+
+    
+
+    SpawnerBody[] getSpawnerBodies()
+    {
+        List<SpawnerBody> spawners = new List<SpawnerBody>();
+        foreach (var item in gameObject.GetComponentsInChildren<SpawnerBody>())
+        {
+            if (item.CompareTag("SpawnerBody"))
+            {
+                spawners.Add(item);
+            }
+        }
+
+        return spawners.ToArray();
+    }
+}
---
+++ Assets/Scripts/CubeSpawner/SpawnerBody.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerBody : MonoBehaviour
+{
+    [SerializeField]
+    private Direction dir;
+    [SerializeField]
+    private GameObject obj;
+    [SerializeField]
+    private float velocity;
+
+    private Vector2 leftTop;
+    private Vector2 rightBot;
+
+
+    void Start()
+    {
+        Vector2 center = gameObject.transform.position;
+        //Debug.Log("Center of Gobj = "+center.x.ToString() + " " + center.y.ToString());
+        Vector3 transform = gameObject.transform.localScale;
+        leftTop = new Vector2(center.x - transform.x/2, center.y + transform.y / 2);
+        rightBot = new Vector2(center.x + transform.x / 2, center.y - transform.y / 2);
+
+        //obj = obj == null ? new Rigidbody2D() : obj;
+    }
+
+    public void spawn()
+    {
+        Vector2 pos = getRandomPosition();
+
+        GameObject newInstance = Instantiate(obj, pos, Quaternion.identity);
+        var vel = getVelocity();
+        newInstance.GetComponent<Rigidbody2D>().velocity = vel;
+    }
+
+    private Vector2 getVelocity()
+    {
+        return getNormVelocity() * velocity;
+    }
+    private Vector2 getNormVelocity()
+    {
+        switch (dir)
+        {
+            case Direction.Down:
+                return new Vector2(0, -1);
+            case Direction.Up:
+                return new Vector2(0, 1);
+            case Direction.Right:
+                return new Vector2(1, 0);
+            case Direction.Left:
+                return new Vector2(-1, 0);
+        }
+        return new Vector2(0, 0);
+    }
+
+    private Vector2 getRandomPosition()
+    {
+        return new Vector2(Random.Range(leftTop.x, rightBot.x), Random.Range(leftTop.y, rightBot.y));
+    }
+}
