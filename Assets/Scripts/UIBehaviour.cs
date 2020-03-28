fb71ff94038104bff532dab06e459f6b602cee61
sinhro200 <sinhro200@users.noreply.github.com>
2020-03-28 13:59:59 +0300

    iss#2

--- Assets/Scripts/UIBehaviour.cs
+++ Assets/Scripts/UIBehaviour.cs
@@ -5,19 +5,7 @@ public class UIBehaviour : MonoBehaviour
 {
     [SerializeField] 
     private Text _scoreText;
-    private void Start()
-    {
 
-    }
-    private void Update()
-    {
-        
-    }
-
-    private void FixedUpdate()
-    {
-        
-    }
     public void setScore(int score)
     {
         Debug.Log("score set to " + score +" in UIBeh") ;
